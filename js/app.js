864b72b4b0399cee65057e3ee6f6a3d640f2d725
Tyler0525 <96202897+Tyler0525@users.noreply.github.com>
2022-02-07 17:41:42 -0500

    pdf

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    // Waits 1000ms after typing "First"
+    strings: ['Web Developer','Software Developer', 'Creator'],
+    typeSpeed:50,
+    backSPeed:30,
+    loop:true,
+});
